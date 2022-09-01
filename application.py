3be2ed8df05881bd966354398e74606283d92543
udorosh <rootdorosh@gmail.com>
2022-09-01 10:30:31 +0300

    v1.3

--- application.py
+++ application.py
@@ -106,7 +106,7 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Congratulation!</h1>
+    <h1>It's working!</h1>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
     <p>This environment is launched with Elastic Beanstalk Python Platform</p>
   </div>
